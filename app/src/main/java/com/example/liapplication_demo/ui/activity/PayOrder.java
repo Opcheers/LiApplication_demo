60f075b3562e49aadb0b96f76ef0853d8444edb1
Longtimenosee-max <76998082+Longtimenosee-max@users.noreply.github.com>
2022-04-06 20:20:37 +0800

    20220406 first submit

---
+++ app/src/main/java/com/example/liapplication_demo/ui/activity/PayOrder.java
@@ -0,0 +1,40 @@
+package com.example.liapplication_demo.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.example.liapplication_demo.R;
+
+public class PayOrder extends AppCompatActivity {
+
+     private RadioButton alipayButton;
+     private RadioButton wx_payButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_order);
+
+        alipayButton = findViewById(R.id.alipay_button);
+        wx_payButton = findViewById(R.id.wx_button);
+
+        alipayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wx_payButton.setChecked(false);
+                alipayButton.setChecked(true);
+            }
+        });
+
+        wx_payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wx_payButton.setChecked(true);
+                alipayButton.setChecked(false);
+            }
+        });
+    }
+}
